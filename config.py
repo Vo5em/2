ed1443b47836b712890746b7f259de419f66e5d4
Vo5em <volkovkir1611@gmail.com>
2025-11-18 11:24:15 +0300

    f

--- config.py
+++
@@ -1,6 +0,0 @@
-TOKEN = "8079935039:AAHRFii6i9BuiPBUbmqr5UBs13TH6w1_O7k"
-access_tokenn = ("vk1.a.-7J6flxSPEV7pHoCWNtuoGzUicLVVmVwlDUxUceHVrJr4XyEShb9GbrJ3hLw5Xjy3-ajC5fIvxQLY-q-RXpn"
-                 "rCmy4HA4NHCPKkYG_VWOr671tI3QP84Bov4I69pzxOkKg3pHizc1YVMIWYU4_uwW6O_xOpn8au2Si_fS6kOru6Zhm"
-                 "dps2j-rh3bTUkvgSEebuXKxlPV1jzPEKgNq2PLEHQ")
-
-BASE_URL = "https://musify.club"
